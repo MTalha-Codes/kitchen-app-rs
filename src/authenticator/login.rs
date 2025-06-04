6e9ea86caad746ce4596267fdc69fa0fe93a83ba
MTalha-Codes <khankkhan123apsacs@gmail.com>
2025-06-04 07:13:05 +0500

    login.h and login.cpp -> login.rs [success]

--- src/authenticator/login.rs
+++ src/authenticator/login.rs
@@ -17,9 +17,10 @@ impl LogIn {
                 let mut cwd = env::current_dir().expect("Failed to fetch to current working directory !");
                 cwd.push("Managed");
                 cwd.push("UserData");
+                cwd.push(_user_name);
                 cwd.set_file_name(_user_name);
                 cwd.set_extension("cred");
-                cwd.to_path_buf().to_str().unwrap().to_string()
+                cwd.to_str().unwrap().to_string()
             };
         
         Ok(LogIn{
@@ -37,7 +38,24 @@ impl LogIn {
        }
     }
     
-    pub fn authenticate_user(&self) {
-        todo!("to be implemented")
+    pub fn authenticate_user(&self) -> Result<&str,&str> {
+        println!("self.file_path = {}",self.file_path);
+        println!("self.user_name = {}",self.user_name);
+        println!("self.password = {}",self.password);
+        if !self.check_user_existence() {
+            Err("Failed to authenticate user (why? user doesn't exists)")
+        } else {
+            let data = fs::read_to_string(&self.file_path).expect("an unexpected error occurred while reading a file");
+            let pipe_index = data.find("|").expect("user file is corrupted");
+            let user_name = &data[0..pipe_index];
+            let password = &data[pipe_index+1..data.len()];
+            let final_enum = if user_name==self.user_name && password == self.password {
+                Ok("Login Successful")
+            }
+            else { 
+                Err("Incorrect Credentials Provided")
+            };
+            final_enum
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,9 @@ mod authenticator;
 
 fn main() -> io::Result<()>  {
    let obj = LogIn::new("Hello","World")?;
+   match obj.authenticate_user() {
+         Ok(_) => println!("Done"),
+         Err(_) => println!("failed")  
+   };
    Ok(())
 }
